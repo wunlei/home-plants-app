3935013f1139addb6ee40f4bad84513da7401ec0
wunlei <confusioncosmos@gmail.com>
2025-01-08 21:28:36 +0300

    Add pages: NotFound page

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,5 +1,17 @@
+import { Link } from "react-router-dom";
+import Button from "@/components/commons/Button/Button";
+import s from "./NotFound.module.scss";
+
 function NotFound() {
-  return <div>Page Not Found</div>;
+  return (
+    <div className={s.container}>
+      <h2 className={s.title}>404</h2>
+      <p className={s.text}>Page Not Found</p>
+      <Link to={"/"} className={s.link}>
+        <Button classes={[s.btn]}>Go to homepage</Button>
+      </Link>
+    </div>
+  );
 }
 
 export default NotFound;
