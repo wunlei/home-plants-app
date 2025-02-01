e5d082d9dfb2a512face8f1cc43ea5af0945b580
wunlei <confusioncosmos@gmail.com>
2025-02-01 18:57:08 +0300

    Add App: toast notifications

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,44 @@
+import { useEffect } from "react";
 import { Outlet } from "react-router-dom";
+import { useAppDispatch, useAppSelector } from "@/state/hooks";
+import { selectNotification } from "@/state/plants/plants.selectors";
+import { resetNotification } from "@/state/plants/plants.slice";
+import { Slide, toast, ToastContainer } from "react-toastify";
 import ErrorBoundary from "@/components/commons/ErrorBoundary";
 
 function App() {
+  const dispatch = useAppDispatch();
+  const notification = useAppSelector(selectNotification);
+
+  useEffect(() => {
+    if (notification.type === "error") {
+      toast.error(notification.text, {
+        onClose: () => {
+          dispatch(resetNotification());
+        },
+      });
+    }
+
+    if (notification.type === "success") {
+      toast.success(notification.text, {
+        onClose: () => {
+          dispatch(resetNotification());
+        },
+      });
+    }
+  }, [dispatch, notification]);
+
   return (
     <ErrorBoundary>
       <Outlet />
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar
+        pauseOnFocusLoss
+        pauseOnHover
+        transition={Slide}
+      />
     </ErrorBoundary>
   );
 }
