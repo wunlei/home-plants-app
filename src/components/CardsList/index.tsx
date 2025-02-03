f1cc0ca2dc16aaafba346cc4a4985a926f1f22e9
wunlei <confusioncosmos@gmail.com>
2025-02-03 20:54:41 +0300

    Change CardsList: loading & error state

--- src/components/CardsList/index.tsx
+++ src/components/CardsList/index.tsx
@@ -1,10 +1,25 @@
 import { useAppSelector } from "@/state/hooks";
-import { selectsPlantsIdsFiltered } from "@/state/plants/plants.selectors";
+import {
+  selectIsError,
+  selectIsLoading,
+  selectsPlantsIdsFiltered,
+} from "@/state/plants/plants.selectors";
 import Card from "@/components/Card";
+import Loader from "@/components/commons/Loader";
 import s from "./CardsList.module.scss";
 
 function CardsList() {
   const plantIds = useAppSelector(selectsPlantsIdsFiltered);
+  const isLoading = useAppSelector(selectIsLoading);
+  const isError = useAppSelector(selectIsError);
+
+  if (isLoading) {
+    return <Loader absolute />;
+  }
+
+  if (isError) {
+    return <h2>Something went wrong</h2>;
+  }
 
   return (
     <div className={s.container}>
