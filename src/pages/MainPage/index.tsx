c12680e813a9a1c45bb8a729fead4383499fba89
wunlei <confusioncosmos@gmail.com>
2025-02-03 20:54:41 +0300

    Change MainPage: loading & error state

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,7 +1,12 @@
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
-import { useAppDispatch } from "@/state/hooks";
-import { updateSearchTerm } from "@/state/plants/plants.slice";
+import { useAppDispatch, useAppSelector } from "@/state/hooks";
+import { getAllPlants, updateSearchTerm } from "@/state/plants/plants.slice";
+import {
+  selectIsCardLoading,
+  selectIsError,
+  selectIsLoading,
+} from "@/state/plants/plants.selectors";
 import AddIcon from "@/assets/add.svg";
 import Button from "@/components/commons/Button";
 import Modal from "@/components/commons/Modal";
@@ -17,6 +22,14 @@ function MainPage() {
   const [searchParams, setSearchParams] = useSearchParams();
   const searchTerm = searchParams.get("search") || "";
 
+  const isLoading = useAppSelector(selectIsLoading);
+  const isCardLoading = useAppSelector(selectIsCardLoading);
+  const isError = useAppSelector(selectIsError);
+
+  useEffect(() => {
+    dispatch(getAllPlants());
+  }, [dispatch]);
+
   const handleSearchTermUpdate = useCallback(
     (value: string) => {
       dispatch(updateSearchTerm(value));
@@ -28,12 +41,17 @@ function MainPage() {
   return (
     <main className={s.main}>
       <h1 className={s.title}>Home plants</h1>
-      <SearchBar value={searchTerm} onChange={handleSearchTermUpdate} />
+      <SearchBar
+        value={searchTerm}
+        disabled={isLoading || isError}
+        onChange={handleSearchTermUpdate}
+      />
       <Button
         icon={<AddIcon />}
         onClick={() => {
           setIsModalOpen(true);
         }}
+        disabled={isLoading || isError}
         classes={[s.btn]}
       >
         Add plant
@@ -42,6 +60,7 @@ function MainPage() {
       <CardsList />
 
       <Modal
+        isLoading={isCardLoading}
         isOpen={isModalOpen}
         onClose={() => {
           setIsModalOpen(false);
