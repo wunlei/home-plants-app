e83359cfb0bc671fb5bfe44b7ace156e727b39ac
wunlei <confusioncosmos@gmail.com>
2025-02-03 20:54:41 +0300

    Change PlantPage: loading & error state

--- src/pages/PlantPage/index.tsx
+++ src/pages/PlantPage/index.tsx
@@ -1,18 +1,47 @@
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { useAppSelector } from "@/state/hooks";
-import { selectPlantById } from "@/state/plants/plants.selectors";
+import { useAppDispatch, useAppSelector } from "@/state/hooks";
+import {
+  selectIsError,
+  selectIsLoading,
+  selectPlantById,
+} from "@/state/plants/plants.selectors";
+import { getPlantById } from "@/state/plants/plants.slice";
+import Loader from "@/components/commons/Loader";
 import Plant from "@/components/commons/Plant";
 import s from "./PlantPage.module.scss";
 
 function PlantPage() {
   const { id } = useParams();
 
+  const dispatch = useAppDispatch();
+  const isLoading = useAppSelector(selectIsLoading);
+  const isError = useAppSelector(selectIsError);
+
+  useEffect(() => {
+    if (id) {
+      dispatch(getPlantById(id));
+    }
+  }, []);
+
   const plant = useAppSelector((state) => selectPlantById(state, id || ""));
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  if (isError) {
+    return (
+      <div className={s.container}>
+        <h2 className={s.title}>Something went wrong</h2>
+      </div>
+    );
+  }
 
   if (!plant) {
     return (
       <div className={s.container}>
-        <h2 className={s.title}>No such plant</h2>
+        <h2 className={s.title}>Plant not found</h2>
       </div>
     );
   }
